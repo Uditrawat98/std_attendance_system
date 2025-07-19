616d56bc49f0b2b2d7d259c6d9149abe33bc1474
Udit Rawat <uditrawat042@gmail.com>
2025-07-19 15:07:05 +0530

    Add files via upload

---
+++ superglobal_variable.php
@@ -0,0 +1,15 @@
+<?php
+$_SESSION["current_user"] = "udit";
+$a="10";
+echo "<pre>";
+//print_r($GLOBALS);
+//print_r($_SERVER);
+//print_r($_REQUEST);
+print_r($_SESSION);
+//print_r($_GET);
+//print_r($POST);
+//print_r($_ENV);
+//print_r($_FILES);
+//print_r($$_COOKIE);
+echo "</pre>";
+?>
